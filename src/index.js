e560309961af2efe7bddf0678fad62e78d381538
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2018-04-15 16:06:23 +0200

    update remote package url

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const scaffoldReadme = (lang) => {
         process.exit()
     }
     
-    const response = await requestP('https://raw.githubusercontent.com/pmcalabrese/riotjs-webpack/master/package.json')
+    const response = await requestP('https://raw.githubusercontent.com/pmcalabrese/node-scaffolder/master/package.json')
 
     const latest_version = JSON.parse(response.body).version;
 
