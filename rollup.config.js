b8abfb85f621d992d02b3f76894c063451639b83
Pachito Marco Calabrese <marco@Pachitos-MacBook-Pro.local>
2018-08-29 23:52:20 +0200

    remove warnings from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
 	{
 		input: 'src/index.js',
-		external: Object.keys(pkg.dependencies),
+		external: [...Object.keys(pkg.dependencies), ...["path", "fs", "process", "util", "child_process"]],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 		],
 		treeshake: true,
 		plugins: [
-			// resolve(),
-			// commonjs()
+			resolve(),
+			commonjs()
 		]
 	}
 ];
